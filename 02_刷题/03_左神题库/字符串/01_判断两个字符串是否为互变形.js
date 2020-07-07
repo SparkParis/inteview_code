9053e9f786f7c86344292fff4ec63504c4b845c7
fengtingting <2674097135@qq.com>
2020-07-08 07:50:50 +0800

    init

---
+++ 02_刷题/03_左神题库/字符串/01_判断两个字符串是否为互变形.js
@@ -0,0 +1,10 @@
+// 字符串总出现的种类和个数都一样称为变形词
+function isChange(s1, s2) {
+  if (!s1 || !s2 || s1.length != s2.length) { 
+    return false
+  }
+  var map = {};
+  for (var i = 0; i < s1.length; i++) { 
+    
+  }
+}
